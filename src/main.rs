bd5f67b04bde1dc577afd589335d3609d212a604
Frederic <fredza801@gmail.com>
2026-05-20 15:06:44 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,33 @@
-#[allow(unused_imports)]
 use std::io::{self, Write};
 
 fn main() {
+    // Affiche le prompt
     print!("$ ");
+
+    // Force l'affichage immédiat du prompt
     io::stdout().flush().unwrap();
+
+    // Variable qui contiendra l'entrée utilisateur
+    let mut input = String::new();
+
+    // Lit la ligne tapée
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Erreur lecture stdin");
+
+    // Supprime les espaces et le retour à la ligne
+    let command = input.trim();
+
+    // Ignore une ligne vide
+    if command.is_empty() {
+        return;
+    }
+
+    // Quitte le programme si la commande est exit
+    if command == "exit" {
+        return;
+    }
+
+    // Affiche l'erreur demandée
+    println!("{}: command not found", command);
 }
